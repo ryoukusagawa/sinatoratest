c5bbce1c265533dc9221f6b911970801fddc979b
ryoukusagawa <s123032@matsu.shimane-u.ac.jp>
2015-08-26 01:26:27 +0000

    contents omission

--- main#4.rb
+++ main#4.rb
@@ -1,7 +1,20 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+=begin
 get '/hello/:name' do
     "hello #{params[:name]}"
 end
+
+
+get '/hello/:name' do |n|
+    "hello #{n}"
+end
+=end
+
+get '/hello/:fname/?:lname?' do |f,l|
+    "hello #{f} #{l}"
+end
+
+
 
