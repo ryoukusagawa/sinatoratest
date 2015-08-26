9e1906b24e52a0ca39daa528ff8f897568b2275c
ryoukusagawa <s123032@matsu.shimane-u.ac.jp>
2015-08-26 02:03:30 +0000

    Regular expression added

--- main#5.rb
+++ main#5.rb
@@ -2,6 +2,12 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-get '/from/*/to/*' do |f,t|
+=begin
+get '/from/*/to/*' do |f,t|#*で省略
     "from #{f} to #{t}"
+end
+=end
+
+get %r{/users/([0-9]*)} do |i|#正規表現を使い、条件に真の場合のみ表示
+    "user id = #{i}"
 end
